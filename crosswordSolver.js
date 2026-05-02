ad4d2aa3a7be76e73383bf428e3dd67a49fd4eb0
melbouzi <mehemmedamineelbouziani969@gmail.com>
2026-05-02 14:16:18 +0100

    fixed mini error

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,17 +1,17 @@
 const emptyPuzzle = `
 2001
 0..0
-1000
+1001
 0..0`
 
 function crosswordSolver(Puzzle,words){
     let Matrix = CreateMatrix(Puzzle)
     let StartIndexesWords = IndexOfFirstEmpty(Matrix);
-    if(typeof StartIndexesWords === "string"){
+    if(StartIndexesWords === "Error"){
         return StartIndexesWords;
     }
     let res = CreateMatrixFromWords(Matrix,StartIndexesWords, words);
-    if (typeof res === "string"){
+    if (res === "Error"){
         return res;
     }
     res = res.map((row) => row.join('')).join('\n');
@@ -31,22 +31,51 @@ function CreateMatrixFromWords(Matrix,StartIndexesWords, words){
 function IndexOfFirstEmpty(matrix){
     let regex = /[^0-9.]/;
     let arr = [];
+    let id = 0
     for(let i = 0; i < matrix.length; i++){
         for(let j = 0; j < matrix[i].length; j++){
             if(matrix[i][j].match(regex) != null){
-                return "error: invalid character in puzzle";
-            } 
+                return "Error";
+            }
             if(matrix[i][j] === '.' || matrix[i][j] === '0'){
                 continue;
-            } 
-            
+            }
+
             arr.push({
                 Value: Number(matrix[i][j]),
+                ID:++id,
                 Y: i,
                 X: j,
-                LineY: matrix.length,
-                LineX: matrix[i].length
+                LineY: Number(matrix[i][j]) > 1?matrix.length:1,
+                LineX: matrix[i].length,
+                F:() =>{
+                    let arrYX = []
+                    if (this.LineY != 1 && this.LineY != matrix.length){
+                        if (matrix[this.Y+1][this.X] != "0"){
+                            arrYX.push(false)
+                        }else{
+                            arrYX.push(true)
+                        }
+                    }else{
+                        arrYX.push(false)
+                    }
+                    if (this.LineX != matrix.length[i]){
+                        if (matrix[this.Y][this.X+1] != "0"){
+                            arrYX.push(false)
+                        }else{
+                            arrYX.push(true)
+                        }
+                    }else{
+                        arrYX.push(false)
+                    }
+                    return arrYX
+                }
+
             });
+            let tableBool = arr[arr.length-1].F()
+            if(tableBool[0] == false && tableBool[1] == false){
+                return "Error"
+            }
         }
     }
     return arr;
